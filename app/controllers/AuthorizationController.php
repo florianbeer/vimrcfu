ef06fe09c86ee9f9757952e15cd43534dea7155c
Florian Beer <fb@42dev.eu>
2014-10-24 13:00:00 +0200

    COMMIT ALL THE FILES!

---
+++ app/controllers/AuthorizationController.php
@@ -0,0 +1,98 @@
+<?php
+
+class AuthorizationController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+    $code = Request::get('code');
+
+    $github = OAuth::consumer('GitHub');
+
+    if ( ! empty($code))
+    {
+      $token = $github->requestAccessToken($code);
+      $result = json_decode($github->request('user'), true);
+
+      dd($result);
+    }
+
+    return Redirect::to((string) OAuth::consumer('GitHub')->getAuthorizationUri());
+  }
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
