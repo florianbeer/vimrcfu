c1cf2b6d2d2535a41967065fda5036c3f4466af1
Florian Beer <fb@42dev.eu>
2014-10-25 21:52:49 +0200

    Use Laravel Str::plural for string pluralization

--- app/views/partials/snippet.blade.php
+++ app/views/partials/snippet.blade.php
@@ -3,7 +3,7 @@
   <h3><a href="{{ URL::route('snippet.show', $snippet['id']) }}">{{{ $snippet['title'] }}}</a></h3>
   <p class="text-muted">
     {{ $snippet['created_at']->diffForHumans() }} by <a href="{{ URL::route('user.show', $snippet['user']->id)  }}">{{ $snippet['user']->name }}</a>,
-    {{ count($snippet->comments) }} {{ Str::plural('comment', count($snippet->comments)) }}
+    {{ count($snippet->comments) }} {{ Str::plural('Comment', count($snippet->comments)) }}
   </p>
   <p>{{{ $snippet['description']  }}}</p>
   @if (Auth::check() && $snippet['user_id'] === Auth::user()->id)
--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -9,19 +9,13 @@
     </div>
     <div class="col-sm-4 col-xs-12">
       <img src="{{ $user->avatar_url }}" class="img-rounded col-sm-6 col-xs-4">
-      <p><span class="badge">{{ $snippets->getTotal() }}</span>
-        @if($snippets->getTotal() == 1)
-        Snippet
-        @else
-        Snippets
-        @endif
+      <p>
+        <span class="badge">{{ $snippets->getTotal() }}</span>
+        {{ Str::plural('Snippet', $snippets->getTotal()) }}
       </p>
-      <p><span class="badge">{{ count($user->comments) }}</span>
-        @if(count($user->comments) == 1)
-        Comment
-        @else
-        Comments
-        @endif
+      <p>
+        <span class="badge">{{ count($user->comments) }}</span>
+        {{ Str::plural('Comment', count($user->comments)) }}
       </p>
       <p><a href="{{ $user->github_url }}">&raquo; GitHub Page</a></p>
     </div>
