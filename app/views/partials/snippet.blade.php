6f14585e0ff2092e4d36c3928f8f1be57fbc9bf9
Florian Beer <fb@42dev.eu>
2014-10-26 08:53:34 +0100

    Display score in partials.snippet

--- app/views/partials/snippet.blade.php
+++ app/views/partials/snippet.blade.php
@@ -11,7 +11,10 @@
   @endif
     <h3><a href="{{ URL::route('snippet.show', $snippet['id']) }}">{{{ $snippet['title'] }}}</a></h3>
     <p class="text-muted">
-      {{ $snippet['created_at']->diffForHumans() }} by <a href="{{ URL::route('user.show', $snippet['user']->id)  }}">{{ $snippet['user']->name }}</a>,
+      {{ $snippet['created_at']->diffForHumans() }} by <a href="{{ URL::route('user.show', $snippet['user']->id)  }}">{{ $snippet['user']->name }}</a>
+    </p>
+    <p class="text-muted small">
+      {{ $snippet->getScore() }} {{ Str::plural('Point', $snippet->getScore()) }},
       {{ count($snippet->comments) }} {{ Str::plural('Comment', count($snippet->comments)) }}
     </p>
     <p>{{{ $snippet['description']  }}}</p>
