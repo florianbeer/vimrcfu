7f9a6063e5bb89410e9b980b51a5b28030cee5bd
Florian Beer <fb@42dev.eu>
2014-10-26 23:58:28 +0100

    Fix snippet meta info display for small screens.

--- app/views/partials/snippet.blade.php
+++ app/views/partials/snippet.blade.php
@@ -10,14 +10,14 @@
   <div class="col-md-10 col-xs-12">
   @endif
     <h3 class="col-xs-12"><a href="{{ URL::route('snippet.show', $snippet['id']) }}">{{{ $snippet['title'] }}}</a></h3>
-    <p class="text-muted small col-xs-6">
+    <p class="text-muted small col-md-6 col-xs-12">
       {{ $snippet['created_at']->diffForHumans() }}<br>
       by <a href="{{ URL::route('user.show', $snippet['user']->id)  }}">{{ $snippet['user']->name }}</a>
       @if (Auth::check() && $snippet['user_id'] === Auth::user()->id)
         <a href="{{ URL::route('snippet.edit', $snippet['id']) }}""button">[edit]</a>
       @endif
     </p>
-    <p class="text-muted small col-xs-6">
+    <p class="text-muted small col-md-6 col-xs-12">
       <i class="fa fa-fire"></i> &nbsp;{{ $snippet->getScore() }} {{ Str::plural('Point', $snippet->getScore()) }}<br>
       <i class="fa fa-comments-o"></i> {{ count($snippet->comments) }} {{ Str::plural('Comment', count($snippet->comments)) }}
     </p>
