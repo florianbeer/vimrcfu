d3c8d3d0d086863d47da6c11c660c8bd6ec03bc9
Florian Beer <fb@42dev.eu>
2014-10-29 21:03:20 +0100

    Remove way/generators

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,6 @@ return array(
     'Illuminate\Validation\ValidationServiceProvider',
     'Illuminate\View\ViewServiceProvider',
     'Illuminate\Workbench\WorkbenchServiceProvider',
-    'Way\Generators\GeneratorsServiceProvider',
     'Artdarek\OAuth\OAuthServiceProvider',
     'Vimrcfu\Providers\TextServiceProvider',
     'Barryvdh\Debugbar\ServiceProvider',
