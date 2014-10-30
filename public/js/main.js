a4dd92f98835d01c515a0afa4129f141c8335b3f
Florian Beer <fb@42dev.eu>
2014-10-30 21:25:40 +0100

    Minor JavaScript optimization
    Always use cached jQuery element.
    Extract callback to function.

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,25 @@
-
 $(function() {
   $('.votelink').on('click', function (e) {
     var link = $(this);
     e.preventDefault();
-    $.get($(this).attr('href'), function (data) {
-      link.removeClass('dark');
-      if(link.hasClass('up')) {
-        $('.votelink.down').addClass('dark');
-      } else {
-        $('.votelink.up').addClass('dark');
-      }
-      $('.points').html(data);
+    $.get(link.attr('href'), function (data) {
+      toggle_link(link, data);
     });
   });
   $('.toggle-visibility').on('click', function () {
     toggle_visibility($(this));
   });
 });
+
+function toggle_link(link, data) {
+  link.removeClass('dark');
+  if(link.hasClass('up')) {
+    $('.votelink.down').addClass('dark');
+  } else {
+    $('.votelink.up').addClass('dark');
+  }
+  $('.points').html(data);
+}
 
 function toggle_visibility(id) {
   elToHide = id.next();
