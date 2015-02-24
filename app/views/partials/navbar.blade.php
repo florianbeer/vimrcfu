264db42d6cdc736aed685cc7050b7449f946825a
Florian Beer <fb@42dev.eu>
2015-02-24 22:32:10 +0100

    Move "Hot" to first position in navbar

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -11,8 +11,8 @@
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li><a href="{{ URL::route('snippet.index') }}"><i class="fa fa-code fa-fw"></i> New</a></li>
         <li><a href="{{ URL::route('snippet.hot') }}"><i class="fa fa-fire fa-fw"></i> Hot</a></li>
+        <li><a href="{{ URL::route('snippet.index') }}"><i class="fa fa-code fa-fw"></i> New</a></li>
         <li><a href="{{ URL::route('snippet.create') }}"><i class="fa fa-plus fa-fw"></i> Add</a></li>
         <li><a href="{{ URL::route('faq') }}"><i class="fa fa-question fa-fw"></i> FAQ</a></li>
       </ul>
